5e3f13be8fdde563d5249ac4e14182e86fe6400b
gamer <gamer@DESKTOP-F7LECQR>
2023-05-04 15:49:44 +0100

    done

--- CMP1903M A01 2223/Card.cs
+++ CMP1903M A01 2223/Card.cs
@@ -8,21 +8,75 @@ namespace CMP1903M_A01_2223
 {
     public class Card
     {
-        string face;
-        string suit;
+        public string Suit { get; set; }
+        public int Rank { get; set; }
 
-        public Card(string faceValue, string suitVal)
+        // Add a property to get the point value of the card
+        public int Points
         {
-            face = faceValue;
-            suit = suitVal;
-
+            get
+            {
+                switch (Rank)
+                {
+                    case 1: // Ace
+                        return 1;
+                    case 11: // Jack
+                        return 10;
+                    case 12: // Queen
+                        return 10;
+                    case 13: // King
+                        return 10;
+                    default: // Other cards
+                        return Rank;
+                }
+            }
         }
-
-        public override string ToString()
-        {
-            return face + "of" + suit;
-        }
-
-
     }
+
+    public static int SortCards(Card[] cards)
+    {
+        // Sort by rank
+        Array.Sort(cards, (a, b) => a.Rank.CompareTo(b.Rank));
+
+        // Sort within each rank by suit
+        for (int i = 0; i < cards.Length - 1; i++)
+        {
+            if (cards[i].Rank == cards[i + 1].Rank)
+            {
+                int j = i + 1;
+                while (j < cards.Length && cards[j].Rank == cards[i].Rank)
+                {
+                    j++;
+                }
+                Array.Sort(cards, i, j - i, (a, b) => a.Suit.CompareTo(b.Suit));
+                i = j - 1;
+            }
+        }
+
+        // Calculate total points earned
+        int totalPoints = 0;
+        foreach (Card card in cards)
+        {
+            totalPoints += card.Points;
+        }
+
+        // Display total points earned
+        Console.WriteLine("You earned {0} points!", totalPoints);
+
+        return totalPoints;
+    }
+
+    Card[] cards = new Card[]
+{
+    new Card { Suit = "Hearts", Rank = 2 },
+    new Card { Suit = "Diamonds", Rank = 7 },
+    new Card { Suit = "Clubs", Rank = 11 },
+    new Card { Suit = "Spades", Rank = 1 },
+    new Card { Suit = "Hearts", Rank = 12 },
+    new Card { Suit = "Diamonds", Rank = 2 },
+    new Card { Suit = "Clubs", Rank = 1 },
+    new Card { Suit = "Spades", Rank = 13 }
+};
+
+    int totalPoints = SortCards(cards);
 }
--- CMP1903M A01 2223/Program.cs
+++ CMP1903M A01 2223/Program.cs
@@ -10,7 +10,56 @@ namespace CMP1903M_A01_2223
     {
         static void Main(string[] args)
         {
+            Card[] cards = new Card[]
+            {
+                new Card { Suit = "Hearts", Rank = 2 },
+                new Card { Suit = "Diamonds", Rank = 7 },
+                new Card { Suit = "Clubs", Rank = 11 },
+                new Card { Suit = "Spades", Rank = 1 },
+                new Card { Suit = "Hearts", Rank = 12 },
+                new Card { Suit = "Diamonds", Rank = 2 },
+                new Card { Suit = "Clubs", Rank = 1 },
+                new Card { Suit = "Spades", Rank = 13 }
+            };
 
+            int totalPoints = SortCards(cards);
+
+            Console.WriteLine("Total points earned: {0}", totalPoints);
+
+            
+            Console.WriteLine("Deal another card? (y/n)");
+            string answer = Console.ReadLine();
+
+            while (answer.ToLower() == "y")
+            {
+                Console.WriteLine("Enter the suit and rank of the card (e.g. Hearts 3):");
+                string input = Console.ReadLine();
+                string[] parts = input.Split(' ');
+                if (parts.Length == 2)
+                {
+                    string suit = parts[0];
+                    int rank;
+                    if (int.TryParse(parts[1], out rank))
+                    {
+                        Card card = new Card { Suit = suit, Rank = rank };
+                        Card[] newCards = cards.Concat(new Card[] { card }).ToArray();
+                        totalPoints = SortCards(newCards);
+                        Console.WriteLine("Total points earned: {0}", totalPoints);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid rank entered.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input entered.");
+                }
+
+                
+                Console.WriteLine("Deal another card? (y/n)");
+                answer = Console.ReadLine();
+            }
         }
     }
 }
